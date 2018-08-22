abfa3aa1772fa7f21c6a625ee4e47467f0265475
王剑波 <jiujiangluck@outlook.com>
2018-08-22 15:51:15 +0800

    --

--- New Version/A1001.cpp
+++ New Version/A1001.cpp
@@ -1,5 +1,27 @@
-//正则表达式和数字转字符串的应用 ；
+/*
+1001 A+B Format (20)
+Calculate a + b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
+
+Input
+
+Each input file contains one test case. Each case contains a pair of integers a and b where -1000000 <= a, b <= 1000000. The numbers are separated by a space.
+
+Output
+
+For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
+
+Sample Input
+
+-1000000 9
+Sample Output
+
+-999,991
+
+*/
+
+//正则表达式和数字转字符串的应用；
 //仅支持c++11以上 ； 
+
 #include<iostream> 
 #include<string>
 #include<algorithm>
@@ -8,10 +30,13 @@
 using namespace std;
 
 int main(){
+
 	std::ios::sync_with_stdio(false);
 	
-	long a,b,c;
+	long a, b, c;
+
 	cin >> a >> b ;
+
 	c = a + b;
 	
 	string s = to_string(c);
--- New Version/A1002.cpp
+++ New Version/A1002.cpp
@@ -1,38 +1,79 @@
+/*
+1002 A+B for Polynomials (25)£¨25 ·Ö£©
+This time, you are supposed to find A+B where A and B are two polynomials.
+
+Input
+
+Each input file contains one test case. Each case occupies 2 lines, and each line contains the information of a polynomial: K N1 a~N1 N2 a~N2 ... NK a~NK, where K is the number of nonzero terms in the polynomial, Ni and a~Ni (i=1, 2, ..., K) are the exponents and coefficients, respectively. It is given that 1 <= K <= 10£¬0 <= NK < ... < N2 < N1 <=1000.
+
+Output
+
+For each test case you should output the sum of A and B in one line, with the same format as the input. Notice that there must be NO extra space at the end of each line. Please be accurate to 1 decimal place.
+
+Sample Input
+
+2 1 2.4 0 3.2
+2 2 1.5 1 0.5
+Sample Output
+
+3 2 1.5 1 2.9 0 3.2
+
+
+*/
+
 #include<iostream>
 #include<iomanip>
 #include<string>
+#include<map>
 #include<algorithm>
 using namespace std;
 
+map<int, float> poly;
+
+void read_data(int n) {
+
+	for (int i = 0; i < n; i++) {
+
+		int e;
+
+		float c;
+
+		cin >> e >> c;
+
+		if (poly.find(e) == poly.end()) poly[e] = c;
+
+		else poly[e] += c;
+
+	}
+
+}
+
 int main(){
+
 	std::ios::sync_with_stdio(false);
-	int n,m,cnt = 0;
-	float r[1002] = {0};
+
+	int n, m, cnt = 0;
 	
 	cin >> n  ;
-	for(int i = 0 ; i  < n ; i++ ){
-		int temp;
-		float ftemp ;
-		cin >> temp >> ftemp;
-		r[temp] += ftemp;
-	}
+
+	read_data(n);
 	
 	cin >> m  ;
-	for(int i = 0 ; i  < m ; i++ ){
-		int temp;
-		float ftemp ;
-		cin >> temp >> ftemp;
-		r[temp] += ftemp;
+
+	read_data(m);
+	
+	for (auto it : poly) {
+
+		if (it.second == 0) cnt++;
+
 	}
 	
-	for(int i = 0 ;  i < 1002; i++){
-		if(r[i] != 0) cnt++;
-	}
-	
-	cout << cnt ;
-	
-	for(int i = 1001 ;  i >= 0 ; i--){
-		if(r[i] != 0)  cout << " " << i << " " << setiosflags(ios::fixed) << setprecision(1) << r[i];
+	cout << poly.size() - cnt ;
+
+	for (auto it = poly.rbegin(); it != poly.rend(); it++) {
+
+		if (it->second != 0) cout << setprecision(1) << setiosflags(ios::fixed) << " " << it->first << " " << it->second;
+
 	}
 	
 	return 0;
